431e8a5c49bea59bb78662ba5b80cf873f5e5f72
yuemengrui <1401190335@qq.com>
2023-07-25 10:47:05 +0800

    update flask app.run

--- ai_server/manage.py
+++ ai_server/manage.py
@@ -1,2 +1,10 @@
 # *_*coding:utf-8 *_*
 from info import app
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+        threaded=False,
+        processes=1
+    )
