79f07cd6cef07298fde158b9a9e4aad3f6f4d3f4
yuemengrui <1401190335@qq.com>
2023-08-01 14:10:32 +0800

    unload embedding

--- ai_server/info/__init__.py
+++ ai_server/info/__init__.py
@@ -9,8 +9,6 @@ from flask_limiter import Limiter
 from sentence_transformers import SentenceTransformer
 from logging.handlers import TimedRotatingFileHandler
 
-__all__ = ['app', 'limiter', 'llm', 'knowledge_vector_store']
-
 
 def setup_logging(log_level):
     logging.basicConfig(level=log_level)
@@ -63,32 +61,33 @@ llm = build_model(model_type=app.config['LLM_MODEL_TYPE'],
                   logger=app.logger)
 
 knowledge_vector_store = KnowledgeVectorStore(vector_store_root_dir=app.config['VS_ROOT_DIR'],
-                                              embedding_model_name_or_path=app.config['EMBEDDING_MODEL_LIST'][0]['model_path'],
+                                              embedding_model_name_or_path=app.config['EMBEDDING_MODEL_LIST'][0][
+                                                  'model_path'],
                                               embedding_device=app.config['EMBEDDING_MODEL_LIST'][0]['device'],
                                               logger=app.logger)
 
-embedding_model_list = []
-
-for i, d in enumerate(deepcopy(app.config['EMBEDDING_MODEL_LIST'])):
-    if i == 0:
-        del d['model_path']
-        del d['device']
-        d.update({"embedding_model": knowledge_vector_store.embeddings.client})
-        embedding_model_list.append(d)
-    else:
-        model_path = d.pop('model_path')
-        device = d.pop('device')
-
-        if model_path:
-            embedding_model = SentenceTransformer(model_name_or_path=model_path, device=device)
-
-            d.update({"embedding_model": embedding_model})
-            embedding_model_list.append(d)
+# embedding_model_list = []
+#
+# for i, d in enumerate(deepcopy(app.config['EMBEDDING_MODEL_LIST'])):
+#     if i == 0:
+#         del d['model_path']
+#         del d['device']
+#         d.update({"embedding_model": knowledge_vector_store.embeddings.client})
+#         embedding_model_list.append(d)
+#     else:
+#         model_path = d.pop('model_path')
+#         device = d.pop('device')
+#
+#         if model_path:
+#             embedding_model = SentenceTransformer(model_name_or_path=model_path, device=device)
+#
+#             d.update({"embedding_model": embedding_model})
+#             embedding_model_list.append(d)
 
 
 from info.modules.SQA import sqa_blu
 app.register_blueprint(sqa_blu)
 from info.modules.Knowledge import knowledge_blu
 app.register_blueprint(knowledge_blu)
-from info.modules.Embedding import embedding_blu
-app.register_blueprint(embedding_blu)
+# from info.modules.Embedding import embedding_blu
+# app.register_blueprint(embedding_blu)
